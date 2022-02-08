a32dc3ebd345694bd5ee2b1ede472894f6da5fb1
Dani Smith <danismith03@gmail.com>
2022-02-08 13:14:34 -0600

    setup layout for pokedex and details

--- pokedex2/src/App.js
+++ pokedex2/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
+import React from 'react';
+import {Routes, Route, Switch} from "react-router-dom";
+import Pokedex from './components/Pokedex';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Pokemon app 2.0
-        </h1>
-        
-      </header>
-    </div>
+    <Routes>
+        <Route exact path="/" element={<Pokedex />}/>
+        </Routes>
   );
 }
 
